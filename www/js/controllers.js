7ae178b2832c8ddb764a102378676aea811784d1
unknown <am790d@TXCDTL122BS998H.ITServices.sbc.com>
2015-06-23 09:17:13 -0500

    Made the profile dependent on an angular factory. Setup for http usage.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('myApp.controllers', [])
 
-	.controller('LoginCtrl', function ($scope, $timeout, $location, $ionicPopup) {
+	.controller('LoginCtrl', function ($scope, $timeout, $location, $ionicPopup, User) {
 
 		// With the new view caching in Ionic, Controllers are only called
 		// when they are recreated or on app start, instead of every page change.
@@ -11,6 +11,7 @@ angular.module('myApp.controllers', [])
 
 		// Form data for the login modal
 		$scope.loginData = {};
+		$scope.user = User;
 
 		// Perform the login action when the user submits the login form
 		$scope.doLogin = function () {
@@ -18,6 +19,8 @@ angular.module('myApp.controllers', [])
 
 			// Simulate a login delay. Remove this and replace with your login
 			// code if using a login system
+			// Update the $scope.user variable with returned info from the login
+
 			$timeout(function () {
 				$scope.closeLogin();
 			}, 1000);
@@ -46,4 +49,24 @@ angular.module('myApp.controllers', [])
 	})
 
 	.controller('NewsfeedSingleCtrl', function ($scope, $stateParams) {
+	})
+
+	// This controller handles editting the user's profile
+	.controller('ProfileCtrl', function($scope, $http, User) {
+		$scope.pass = {first: "", second: "", update: false};
+		$scope.user = User;
+		$scope.info = {name: angular.copy(User.name), interests: angular.copy(User.interests), email: angular.copy(User.email), phone: angular.copy(User.phone)};
+
+		// Once you click the save button
+		$scope.saveProfileChanges = function() {
+			if ($scope.pass.first != "" && $scope.pass.first == $scope.pass.second) {
+				$scope.pass.update = true;
+				alert("New Password: "+$scope.pass.first+"\n");
+			}
+			alert("Interests: "+$scope.info.interests+"\nEmail: "+$scope.info.email+"\nPhone: "+$scope.info.phone);
+
+			// Upon successful http call, update $scope.user
+
+			$scope.pass = {first: "", second: "", update: false};
+		};
 	});
