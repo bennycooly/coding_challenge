474a8301a3bcba7acfbecadb512937d73c064275
unknown <bf6510@TXCDTL159BS998H.ITServices.sbc.com>
2015-06-23 09:32:23 -0500

    working on authentication and animations for login

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('myApp', ['ionic', 'myApp.controllers'])
+angular.module('myApp', ['ionic', 'myApp.controllers', 'ngCookies'])
 
 	.run(function ($ionicPlatform) {
 		$ionicPlatform.ready(function () {
@@ -26,8 +26,7 @@ angular.module('myApp', ['ionic', 'myApp.controllers'])
 			.state('app', {
 				url: "/app",
 				abstract: true,
-				templateUrl: "templates/menu.html",
-				controller: 'LoginCtrl'
+				templateUrl: "templates/menu.html"
 			})
 
 			.state('login', {
@@ -118,6 +117,9 @@ angular.module('myApp', ['ionic', 'myApp.controllers'])
 					}
 				}
 			});
+
+
+
 
 		// if none of the above states are matched, use this as the fallback
 		$urlRouterProvider.otherwise('/login')
